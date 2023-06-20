d90fc541123b710f41b3a56d89b40bab6cd8c4be
FenrirDrage <garfield.alves@gmail.com>
2023-06-20 13:19:02 +0100

    new addons
    add recicle menu
    and correct main menu

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function selectItem(item, event) {
     menuItems.forEach(item => item.classList.remove('selected'));
 
     // Add the 'selected' class to the clicked menu item
-    const selectedItem = event.target;
+    const selectedItem = event.currentTarget;
     selectedItem.classList.add('selected');
 
     const topBar = document.getElementById('topbar');
@@ -23,12 +23,102 @@ function selectItem(item, event) {
         heading.textContent = 'teste local';
         contentContainer.appendChild(heading);
 
+        /*// Fetch data from Firestore and create cards
+        db.collection('your_collection_name').get()
+        .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            const card = createCard(doc.data());
+            contentContainer.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data: ', error);
+        });*/ //Codigo a alterar para ir buscar a data á firestore
+
     } else if (item === 'Recicle') {
         // DOM layout changes for 'Recicle' item
         // Example: Show specific content or update styles
         const heading = document.createElement('h1');
         heading.textContent = 'recicla-me';
         contentContainer.appendChild(heading);
+
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container', 'grid', 'gap-3');
+    
+        // Create the cards for each type of "Eco ponto"
+        const ecoPontos = [
+        'Eco ponto Amarelo',
+        'Eco ponto Azul',
+        'Eco ponto Verde',
+        'Eco ponto Vermelho',
+        'Eco ponto Castanho'
+        ];
+
+        // Mapping between card names and colors
+        const colorMap = {
+            'Eco ponto Amarelo': '#f1c40f',
+            'Eco ponto Azul': '#3498db',
+            'Eco ponto Verde': '#2ecc71',
+            'Eco ponto Vermelho': '#e74c3c',
+            'Eco ponto Castanho': '#755353'
+        };
+
+        //create in DOM the cards
+        ecoPontos.forEach(title => {
+        const cardLink = document.createElement('a');
+        cardLink.href = '';
+        cardLink.classList.add('menu-link');
+        const card = document.createElement('div');
+        card.classList.add('card', 'p-2', 'g-col-6');
+        const cardTitle = document.createElement('h3');
+        cardTitle.textContent = title;
+        card.style.backgroundColor = colorMap[title]; // Set the background color dynamically
+    
+        card.appendChild(cardTitle);
+        cardLink.appendChild(card);
+        cardContainer.appendChild(cardLink);
+
+        // Add click event listener to each card
+        cardLink.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // Show the modal and populate it with the card title
+        const modal = document.getElementById('myModal');
+        const modalTitle = modal.querySelector('.modal-title');
+        modalTitle.textContent = title;
+        modal.style.display = 'block';
+        });
+      });
+
+    // Get the modal element
+    const modal = document.getElementById("myModal");
+
+    // Get the close button element
+    const closeButton = modal.querySelector(".close");
+
+    // Function to open the modal
+    function openModal(title) {
+    const modalTitle = modal.querySelector(".modal-title");
+    modalTitle.textContent = title;
+    modal.style.display = "block";
+    }
+
+    // Function to close the modal
+    function closeModal() {
+    modal.style.display = "none";
+    }
+
+    // Event listener for the close button
+    closeButton.addEventListener("click", closeModal);
+
+    // Event listener to close the modal when clicking outside of it
+    window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+    });
+
+    contentContainer.appendChild(cardContainer);
 
     } else if (item === 'Cards') {
         // DOM layout changes for 'Cards' item
@@ -69,6 +159,9 @@ function selectItem(item, event) {
         heading.textContent = 'pokerface';
         contentContainer.appendChild(heading);
     }
+
+    // Reset the scroll position to the top of the content container
+  contentContainer.scrollTop = 0;
 }
 
 // Default selection
